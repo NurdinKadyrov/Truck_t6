4d4c76a4428df5003e589dedaa3d9b601d805f4e
ACER <k>
2022-07-12 21:09:03 +0600

    commit

--- src/com/company/service/TruckService.java
+++ src/com/company/service/TruckService.java
@@ -3,4 +3,7 @@ package com.company.service;
 import com.company.service.impl.TruckServiceImpl;
 
 public class TruckService implements TruckServiceImpl {
+    public static void main(String[] args) {
+
+    }
 }
